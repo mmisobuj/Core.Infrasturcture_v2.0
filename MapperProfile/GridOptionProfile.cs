db2b2250424e48d7a175379fdd700ac79db44e24
sobuj <sobuj.ah@gmail.com>
2023-09-08 00:48:03 +0600

    no

---
+++ MapperProfile/GridOptionProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Infrastructure.Grid;
+using Core.Infrastructure.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Core.Infrastructure.Grid.KendoGridFilter;
+
+namespace Core.Infrastructure.MapperProfile
+{
+    public class GridOptionProfile   : Profile
+    {
+        public GridOptionProfile()
+        {
+            CreateMap<GridOptions, GridOptionsV2>();
+            CreateMap<KendoGridFilter.GridSort, Sort>();
+            CreateMap<KendoGridFilter.GridFilters, Filter>();
+            CreateMap<KendoGridFilter.GridFilter, Filter>();
+
+            
+
+        }
+    }
+}
